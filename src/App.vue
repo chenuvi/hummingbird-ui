9e162b31af2bb6aa780cd0cec2f97004c945463a
chenuvi <chenuvi@outlook.com>
2021-03-16 21:25:38 +0800

    Init: init program

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>hello world</div>
+  <Chenuvi></Chenuvi>
+</template>
+
+<script>
+import Chenuvi from "./components/Chenuvi.vue";
+export default {
+  components: { Chenuvi },
+};
+</script>
+
+<style></style>
---
+++ src/components/Chenuvi.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        我的第一个组件
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style  scoped>
+
+</style>
